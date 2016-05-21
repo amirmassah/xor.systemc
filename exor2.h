f8b39bfa8a42521f6483f60defef9a7c127e8354
VDS09 <VDS09>
2016-05-21 16:26:58 +0200

    dd

--- exor2.h
+++ exor2.h
@@ -7,3 +7,43 @@
 // inputs: bool A, B
 // output bool F
 
+SC_MODULE(exor2)
+{
+	sc_in <bool> A, B;
+	sc_out <bool> F;
+
+
+	void do_exor2()
+	{
+	}
+
+	SC_CTOR()
+	{
+		nand2 n1("n1"),
+			  n2("n2"),
+			  n3("n3"),
+			  n4("n4");
+
+		sc_signal <bool> n1n2n3_sig, n2n4_sig, n3n4_sig;
+		
+		n1.A(A);
+		n1.B(B);
+		n1.F(n1n2n3_sig);
+
+		n2.A(A);
+		n2.B(n1n2n3_sig);
+		n2.F(n2n4_sig);
+
+		n3.A(n1n2n3_sig);
+		n3.B(B);
+		n3.F(n3n4_sig);
+
+		n4.A(n2n4_sig);
+		n4.B(n3n4_sig);
+		n4.F(F);
+
+		//SC_METHOD(do_exor2);
+		//sensitive << A << B;
+	}
+};
+
